ce7ab080915cad9e5818dd62e55e52e488e91c4f
gonzalo lara <gonzaloflara@gmail.com>
2023-05-21 19:20:53 -0300

    sudoku checker done

--- sudoku.py
+++ sudoku.py
@@ -1,5 +1,26 @@
 # pylint: disable=missing-docstring
 
-
 def sudoku_validator(grid):
-    pass  # YOUR CODE HERE
+    rows = [set() for _ in range(9)]
+    cols = [set() for _ in range(9)]
+    boxes = [set() for _ in range(9)]
+
+    for i in range(9):
+        for j in range(9):
+            if grid[i][j] != ".":
+                num = int(grid[i][j])
+
+                if num in rows[i]:
+                    return False
+                rows[i].add(num)
+
+                if num in cols[j]:
+                    return False
+                cols[j].add(num)
+
+                box_index = (i // 3) * 3 + j // 3
+                if num in boxes[box_index]:
+                    return False
+                boxes[box_index].add(num)
+
+    return True
